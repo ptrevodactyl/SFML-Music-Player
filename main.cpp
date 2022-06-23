baed04f83a47c12b6db10d205a60b8afa806959f
ptrevordacty1 <trevor.holland@wsu.edu>
2022-06-23 11:39:19 -0700

    Added background

--- main.cpp
+++ main.cpp
@@ -3,22 +3,27 @@
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Trevor's Music App", sf::Style::Close | sf::Style::Resize);
+    sf::RectangleShape background = sf::RectangleShape((sf::Vector2f) window.getSize());
+    background.setFillColor(sf::Color(86, 65, 65));
+    sf::Event evnt;
 
     // main activity loop
     while(window.isOpen()) {
         // window event handling
-        sf::Event winEvent;
-        while(window.pollEvent(winEvent)) {
-
-            switch(winEvent.type) {
+        while(window.pollEvent(evnt)) {
+            switch(evnt.type) {
                 case sf::Event::Closed:
                     window.close();
                     break;
-                case sf::Event::Resized:
-                    std::cout << winEvent.size.width << ", " << winEvent.size.height << std::endl;
-                    break;
+                
+                case sf::Event::Resized | sf::Event::GainedFocus | sf::Event::LostFocus:
+                    background.setSize((sf::Vector2f) window.getSize());
             }
         }
+
+        window.clear();
+        window.draw(background);
+        window.display();
     }
 
     return 0;
